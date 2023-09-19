545a7c5be01d2384d82550f6d6b19fb71161cb22
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-19 13:35:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let browserName = navigator.appName;
+
+let version= navigator.appVersion ;
+
+let section = document.getElementById("browser-info");
+section.innerText =`You are using ${browserName} version ${version}`;
